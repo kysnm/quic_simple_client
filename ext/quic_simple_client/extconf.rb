aa8d50c5214f01819ae406808c01520625796aaf
kysnm <tokyoincidents.g@gmail.com>
2016-12-20 21:49:22 +0900

    :sparkles: Add dependencies compile procedure

--- ext/quic_simple_client/extconf.rb
+++ ext/quic_simple_client/extconf.rb
@@ -1,3 +1,27 @@
 require "mkmf"
 
+RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
+
+$CFLAGS << " #{ENV["CFLAGS"]}"
+$CFLAGS << " -g"
+$CFLAGS << " -O3" unless $CFLAGS[/-O\d/]
+$CFLAGS << " -Wall -Wno-comment"
+
+CWD = File.expand_path(File.dirname(__FILE__))
+QUIC_DIR = File.join(CWD, 'libquic')
+QUIC_BUILD_DIR = File.join(QUIC_DIR, 'build')
+
+$LDFLAGS << " -L#{QUIC_BUILD_DIR}"
+
+Dir.mkdir(QUIC_BUILD_DIR) if !Dir.exists?(QUIC_BUILD_DIR)
+
+Dir.chdir QUIC_BUILD_DIR do
+  system 'cmake ..'
+  system 'make -j 4'
+end
+
+LIBDIR     = RbConfig::CONFIG['libdir']
+
+$LOCAL_LIBS << '-lquic'
+
 create_makefile("quic_simple_client/quic_simple_client")
